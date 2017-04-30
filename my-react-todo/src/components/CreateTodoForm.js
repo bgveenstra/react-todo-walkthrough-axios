c53b7427563b916c0142048fcf3958a4d591825a
bgveenstra <bgveenstra@gmail.com>
2017-04-29 22:17:19 -0700

    add onInputChange method to CreateTodoForm class

--- my-react-todo/src/components/CreateTodoForm.js
+++ my-react-todo/src/components/CreateTodoForm.js
@@ -10,12 +10,21 @@ class CreateTodoForm extends Component {
       todo: ''
     }
   }
+
+  onInputChange(event){
+    console.log('create todo input changed')
+    this.setState({
+      todo: event.target.value
+    })
+  }
+
   render(){
     return (
       <div className='createForm todoForm'>
         <h2>Create Todo Here!</h2>
         <form>
           <input
+            onChange={event => this.onInputChange(event)}
             placeholder='Write a todo here ...'
             type='text'
             value={this.state.todo} />
