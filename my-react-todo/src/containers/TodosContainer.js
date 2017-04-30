0491651e0008a48abab79353e305f20cea3a0490
bgveenstra <bgveenstra@gmail.com>
2017-04-29 20:25:09 -0700

    replace single Todo test with TodoList test in TodosContainer render

--- my-react-todo/src/containers/TodosContainer.js
+++ my-react-todo/src/containers/TodosContainer.js
@@ -1,20 +1,21 @@
 // src/containers/TodosContainer.js
 import React, {Component} from 'react'
 import TodoModel from '../models/Todo'
-import Todo from '../components/Todo'
+import TodoList from '../components/TodoList'
 
 class TodosContainer extends Component {
   render(){
-  	// check that TodoModel AJAX call gets todo data
-  	TodoModel.all().then( (res) => {
+    // check that TodoModel AJAX call gets todo data
+    TodoModel.all().then( (res) => {
       console.log(res);
     })
     return (
       <div className='todosContainer'>
         <h2>This is the todos container</h2>
-        <Todo
-          key={"58e3c74c93075e0011489f04"}
-          todo={{"_id":"58e3c74c93075e0011489f04","body":"Take a walk outside","priority":5,"completed":false,"__v":0}}/>
+        <TodoList
+          todos={
+            [{"_id":"58e3c74c93075e0011489f02","body":"Wash the dishes","priority":4,"completed":false,"__v":0},{"_id":"58e3c74c93075e0011489f07","body":"Update resume with new skills","priority":2,"completed":false,"__v":0},{"_id":"58e3c74c93075e0011489f05","body":"Buy nutritious groceries for the week","priority":2,"completed":false,"__v":0}]
+          } />
       </div>
     )
   }
